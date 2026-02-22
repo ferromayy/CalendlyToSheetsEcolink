9f76e1e22025a23659f00f73b5b11e7a4a62c596
fernaaaa <ferromay.is@gmail.com>
2026-02-22 18:50:52 -0300

    added debbugs

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,18 @@ import { appendToSheet } from "./googleSheets";
 
 dotenv.config();
 
+
+console.log("🔥 SERVER BOOTING...");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
+
+app.use((req, _res, next) => {
+  console.log("➡️ REQUEST:", req.method, req.url);
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("Calendly Webhook Server is running!");
